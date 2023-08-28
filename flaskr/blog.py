0c028a9235562e2d44b00e704ccffbcd3a39c9cc
Kate Lueders <kelueders@gmail.com>
2023-08-28 17:22:47 -0500

    created details route

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -89,7 +89,7 @@ def update(id):
             db.commit()
             return redirect(url_for('blog.index'))
         
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', post = post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
@@ -99,4 +99,12 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+@bp.route('/<int:id>/details', methods=['POST'])
+@login_required
+def details(id):
+    post = get_post(id)
+
+    return render_template('blog/details.html', post = post)
+
 
